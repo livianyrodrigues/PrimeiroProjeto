27858a3ab9702af573930ee09161ff4994a4c704
Liviany Reis <livianyrodrigues@hotmail.com>
2014-05-18 19:40:21 -0300

    Primeiro commit

---
+++ Plano.java
@@ -0,0 +1,33 @@
+package com.claro.sac;
+
+public class Plano {
+
+	private String tipo;
+	private String nome;
+	private double valor;
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+}
